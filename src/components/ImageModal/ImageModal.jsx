9381a4f9204919e46f4ec3a665d40807a477d980
Vitalii Dylovyi <vitalii.dylovyi@gmail.com>
2025-08-19 00:59:43 +0300

    fixes

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -5,11 +5,11 @@ const ImageModal = ({ image, onClose }) => {
     <Modal
       isOpen={!!image}
       shouldCloseOnEsc
-      style={{ content: { padding: 0, inset: 80 } }}
+      style={{ content: { padding: 0, inset: 80, overflow: 'hidden' } }}
       onRequestClose={onClose}
       contentLabel='Image Modal'
     >
-      {image && <img src={image.urls.regular} style={{ minWidth: '100%' }} />}
+      {image && <img src={image.urls.regular} style={{ minWidth: '100%', height: '100%', objectFit: 'contain' }} />}
     </Modal>
   );
 };
