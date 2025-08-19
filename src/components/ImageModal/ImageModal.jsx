5c933aed83f6591ac4176e912ec02cc6a30f60d1
Vitalii Dylovyi <vitalii.dylovyi@gmail.com>
2025-08-19 10:56:04 +0300

    move inline styles to css module

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,15 +1,16 @@
 import Modal from 'react-modal';
+import styles from './ImageModal.module.cs';
 
 const ImageModal = ({ image, onClose }) => {
   return (
     <Modal
       isOpen={!!image}
       shouldCloseOnEsc
-      style={{ content: { padding: 0, inset: 80, overflow: 'hidden' } }}
       onRequestClose={onClose}
       contentLabel='Image Modal'
+      style={{ content: { padding: 0, inset: 80, overflow: 'hidden' } }}
     >
-      {image && <img src={image.urls.regular} style={{ minWidth: '100%', height: '100%', objectFit: 'contain' }} />}
+      {image && <img src={image.urls.regular} className={styles['image-full']} />}
     </Modal>
   );
 };
